0e8f38c581219c9630c68bb740afa7dedd357b8f
tomomo <eclairpark@gmail.com>
2021-02-22 00:47:12 +0900

    welcomeをphpinfoに差し替え

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,11 +1,29 @@
 <?php
+/**
+ * Home.php
+ *
+ * @package App\Controllers
+ * @author  tomomo <eclairpark@gmail.com>
+ * @license http://www.opensource.org/licenses/mit-license.html  MIT License
+ */
 
 namespace App\Controllers;
 
+/**
+ * Class Home
+ *
+ * @package App\Controllers
+ */
 class Home extends BaseController
 {
+	/**
+	 * ホーム画面表示
+	 *
+	 * @return void
+	 */
 	public function index()
 	{
-		return view('welcome_message');
+		phpinfo();
+		// return view('welcome_message');
 	}
 }
